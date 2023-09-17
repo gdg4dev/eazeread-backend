5d1485b8987f60c0ed37e7265dc6d36834744785
Aniket Hirpara <gdg4dev@gmail.com>
2023-09-17 03:41:33 -0400

    cors error patch

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { getGPTResponse } = require('./utils/openai');
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors())
 app.use(express.json());
 const axios = require('axios');
 const cheerio = require('cheerio');
